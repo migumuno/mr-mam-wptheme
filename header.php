d00ffe829018b91edc14953a462f4efbcfb3515a
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-01 13:58:49 +0200

    Modificación texto cabecera

--- header.php
+++ header.php
@@ -26,6 +26,7 @@
 		<div class="row container">
 			<div class="col-xs-6 header__logo">
 				<h1>< Mr. Mam ></h1>
+				<span>Diseño y programación</span>
 			</div>
 			<div class="col-xs-6 header__menu">
 				<span class="hidden-md hidden-lg">☰ Menú</span>
