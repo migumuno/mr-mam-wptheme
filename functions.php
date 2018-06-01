8a726ac8fb2a3c0a42f8395d9800367a21e22753
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-01 20:04:23 +0200

    Añado librería jQuery

--- functions.php
+++ functions.php
@@ -140,6 +140,8 @@ function mr_mam_scripts() {
 
 	wp_enqueue_script( 'mr-mam-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), '3.3.1' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
