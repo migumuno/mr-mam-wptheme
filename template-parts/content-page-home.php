9cdc6a964ff4be7bb41084ae0045f1d29be89383
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-05 11:29:15 +0200

    Reduzco el tiempo del autoplay del slider

--- template-parts/content-page-home.php
+++ template-parts/content-page-home.php
@@ -176,7 +176,7 @@
 								slidesToShow: 4,
 								slidesToScroll: 1,
 								autoplay: true,
-								autoplaySpeed: 2000,
+								autoplaySpeed: 1000,
 								responsive: [
 									{
 									breakpoint: 1024,
