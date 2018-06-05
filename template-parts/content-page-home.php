06c68f089f8a709d7321dc9cea7a21f4416f9c9f
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-05 17:52:03 +0200

    Ajuste de estilos en las habilidades de trabajo

--- template-parts/content-page-home.php
+++ template-parts/content-page-home.php
@@ -117,7 +117,7 @@
 			</div>
 			<div class="work-wrapper__skills aire">
 				<div class="col-md-2 work__skills-title"><h3>Habilidades</h3></div>
-				<div class="col-xs-6 col-md-5 work__skills-list row">
+				<div class="col-sm-6 col-md-5 work__skills-list row">
 					<p class="work__skills-list__title"><b>Back-end</b></p>
 					<ul>
 						<li class="work-wrapper__skills-list__item">PHP</li>
@@ -129,7 +129,7 @@
 						<li class="work-wrapper__skills-list__item"><span>L</span> WordPress</li>
 					</ul>
 				</div>
-				<div class="col-xs-6 col-md-5 work__skills-list row">
+				<div class="col-sm-6 col-md-5 work__skills-list row">
 					<p class="work__skills-list__title"><b>Front-end</b></p>
 					<ul>
 						<li class="work-wrapper__skills-list__item">JavaScript</li>
