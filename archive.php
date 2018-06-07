b720329705551e4d80dc166d44558d60ea12b250
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-07 13:37:48 +0200

    Cambio estructura de los posts

--- archive.php
+++ archive.php
@@ -27,12 +27,10 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				echo '<div class="mr-mam-blog-posts__post col-xs-12 col-sm-6 col-md-4 col-lg-3">
+					'.get_the_post_thumbnail().'
+					<h3><a href="'.get_permalink().'">'.get_the_title().'</a></h3>
+				</div>';
 
 			endwhile;
 
