d2dec61d5c61df53758c5b3a1c8d2bee07814bbb
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-06-10 09:40:09 +0200

    Ajustes de contenidos

--- template-parts/content-page-home.php
+++ template-parts/content-page-home.php
@@ -74,7 +74,7 @@
 			</div>
 			<div class="col-md-6 welcome-wrapper__text">
 				<p>Hola, Soy Miguel Ángel Muñoz Viejo.</p>
-				<p>Contrátame para construir una web alucinante. Echa un ojo sobre quién soy, leete alguno de mis artículos o ponte en contacto conmigo y nos tomamos un café.</p>
+				<p>Contrátame para construir una web alucinante. Echa un ojo sobre quién soy, léete alguno de mis artículos o ponte en contacto conmigo y nos tomamos un café.</p>
 				<?php do_shortcode( '[formas_contacto]' ) ?>
 			</div>
 		</div>
@@ -98,7 +98,7 @@
 			<div class="work-wrapper__text aire">
 				<h2>Trabajo</h2>
 				<p>Soy full stack developer, lo que significa que tengo conocimientos tanto de la parte de back-end como de la parte de front-end. Defensor absoluto del código libre e intentando compartir cada día más cosas con el resto del mundo.</p>
-				<p>He sido director de desarrollo durante varios años en diversas empresas y he dirigido y ejecutado multitud de proyectos de diferentes tipos y con diferentes tecnologías. Tras la carrera, inicié mi camino desarrollando para web con PHP y en el camino me enamoré de WordPress (al principio más odio que amor ;)). En el front-end HTML5, CSS3 y JavaScript son mi día a día y me fascina toda la tecnología que está saliendo entorno a este último (NodeJS, React, AngularJS…).</p>
+				<p>He sido director de desarrollo durante varios años en diversas empresas y he dirigido y ejecutado multitud de proyectos de diferentes tipos y con diferentes tecnologías. Tras la carrera, inicié mi camino desarrollando para web con PHP y en el camino me enamoré de WordPress (al principio más odio que amor ;)). En el front-end HTML5, CSS3 y JavaScript son mi día a día y me fascina toda la tecnología que está saliendo en torno a este último (NodeJS, React, AngularJS…).</p>
 			</div>
 			<div class="work-wrapper__skills aire">
 				<div class="col-md-2 work__skills-title"><h3>Habilidades</h3></div>
@@ -201,7 +201,7 @@
 			<h2>Sobre mi</h2>
 			<h3>Emprendedor & Friki</h3>
 			<p>Emprendedor de nacimiento y friki de la vida, me puedo pasar horas e incluso días pegado a un ordenador (muchas Euskal y Campus party y fast coding) aprendiendo nuevas cosas o desarrollando nuevos proyectos. Mi frikismo no solo se queda en lo profesional, mi casa está llena de drones, robot limpiador, Raspberry, domótica… me vuelve loco la tecnología. Soy una persona <a href="https://youtu.be/EaS7jybfmcM" target="_blank">multipotencial</a> pero encontré mi pasión en la programación ya que me permite emprender muchos de los proyectos que me planteo.
-Si me preguntas por mis hobbies, me encontrarás en alguna pista de pádel, un campo de golfo o pegando tiros en un campo de airsoft (me encantan la estrategia y el compañerismo).</p>
+Si me preguntas por mis hobbies, me encontrarás en alguna pista de pádel, un campo de golf o pegando tiros en un campo de airsoft (me encantan la estrategia y el compañerismo).</p>
 		</div>
 	</section>
 
