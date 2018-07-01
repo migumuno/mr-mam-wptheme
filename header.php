3d60834b18b25a36685d7af4f07dd9318fedce59
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-07-01 14:25:21 +0200

    Añado icons para favicon e imágenes en apps

--- header.php
+++ header.php
@@ -16,6 +16,23 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?=get_stylesheet_directory_uri()?>/img/icos/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="<?=get_stylesheet_directory_uri()?>/img/icos/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?=get_stylesheet_directory_uri()?>/img/icos/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?=get_stylesheet_directory_uri()?>/img/icos/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?=get_stylesheet_directory_uri()?>/img/icos/favicon-16x16.png">
+	<link rel="manifest" href="<?=get_stylesheet_directory_uri()?>/img/icos/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120032934-1"></script>
